07b00e4f2ec4cb2a2a652c525fbb3ceb40565cd6
ligai <ligai@egova.com.cn>
2019-08-14 21:16:03 +0800

    [M]提交站点

---
+++ dist/precache-manifest.f243306a11d16b49e920845331f22000.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff8cbb157ba9a8cfacc",
+    "url": "./vendors.ffe76721.chunk.css"
+  },
+  {
+    "revision": "aff8cbb157ba9a8cfacc",
+    "url": "./vendors.c29ef91e.async.js"
+  },
+  {
+    "revision": "043b32d1132c2c20f42e",
+    "url": "./layouts__BasicLayout.b1bb6c50.chunk.css"
+  },
+  {
+    "revision": "043b32d1132c2c20f42e",
+    "url": "./layouts__BasicLayout.1ee2638b.async.js"
+  },
+  {
+    "revision": "dfe419c6fe189db6e3dd",
+    "url": "./p__404.2dce332e.chunk.css"
+  },
+  {
+    "revision": "dfe419c6fe189db6e3dd",
+    "url": "./p__404.4847e9c2.async.js"
+  },
+  {
+    "revision": "31044ed1434f8aee7d68",
+    "url": "./p__Mark__MarkObject__$id.3fa9e487.chunk.css"
+  },
+  {
+    "revision": "31044ed1434f8aee7d68",
+    "url": "./p__Mark__MarkObject__$id.e688ed6a.async.js"
+  },
+  {
+    "revision": "06867ae7c64e8aaab461",
+    "url": "./p__Mark__MarkType.87fa7dd1.chunk.css"
+  },
+  {
+    "revision": "06867ae7c64e8aaab461",
+    "url": "./p__Mark__MarkType.738e189f.async.js"
+  },
+  {
+    "revision": "451b0cdb82f699b1ae00",
+    "url": "./p__Welcome.cbe88e20.async.js"
+  },
+  {
+    "revision": "325436b54c185a0b6a9c",
+    "url": "./umi.52a5d24d.css"
+  },
+  {
+    "revision": "325436b54c185a0b6a9c",
+    "url": "./umi.07e267de.js"
+  },
+  {
+    "revision": "a4ef60fe13a71a93e39ca954aa746007",
+    "url": "./index.html"
+  },
+  {
+    "revision": "22a4c1f96d60a77be65a9db3d51b4ca9",
+    "url": "./favicon.png"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "./icons\\icon-128x128.png"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "./icons\\icon-192x192.png"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "./icons\\icon-512x512.png"
+  },
+  {
+    "revision": "4b6c7d5f24e137b9b98a90e2b070e452",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "./pwacompat.min.js"
+  }
+];
